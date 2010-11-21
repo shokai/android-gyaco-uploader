7a37fa4c8e0463ed3856527c91a812356d5c2f68
Sho Hashimoto <hashimoto@shokai.org>
2010-11-21 17:18:29 +0900

    content:// -> filepath

--- src/org/shokai/gyaco/Main.java
+++ src/org/shokai/gyaco/Main.java
@@ -1,7 +1,11 @@
 package org.shokai.gyaco;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,29 @@ public class Main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        trace("start");
+        Uri soundUri = null;
+
+        try{
+            soundUri = Uri.parse(getIntent().getExtras().get("android.intent.extra.STREAM").toString());
+            trace(soundUri.toString());
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        if (soundUri != null) {
+            trace("from share menu");
+            Cursor c = this.getContentResolver().query(soundUri, null, null, null, null);
+            c.moveToFirst();
+            String filename = c.getString(c.getColumnIndex(MediaStore.MediaColumns.DATA));
+            trace(filename);
+        }
+        else{
+            trace("run as standalone app");
+        }
+    }
+    
+    public void trace(String str){
+        Log.v("GyacoUploader", str);
     }
 }
